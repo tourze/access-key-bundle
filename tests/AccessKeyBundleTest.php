9eea69b88510aa96f1207a04c1c0901caaa6dfce
github-ci <you@example.com>
2025-11-18 23:19:17 +0800

    fix error

--- tests/AccessKeyBundleTest.php
+++ tests/AccessKeyBundleTest.php
@@ -2,13 +2,9 @@
 
 namespace Tourze\AccessKeyBundle\Tests;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use Tourze\AccessKeyBundle\AccessKeyBundle;
-use Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle;
-use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
-use Tourze\DoctrineTrackBundle\DoctrineTrackBundle;
 use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 
 /**
@@ -18,19 +14,4 @@ use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 #[RunTestsInSeparateProcesses]
 final class AccessKeyBundleTest extends AbstractBundleTestCase
 {
-    public function testGetBundleDependencies(): void
-    {
-        $dependencies = AccessKeyBundle::getBundleDependencies();
-
-        $this->assertIsArray($dependencies);
-        $this->assertArrayHasKey(DoctrineBundle::class, $dependencies);
-        $this->assertArrayHasKey(DoctrineSnowflakeBundle::class, $dependencies);
-        $this->assertArrayHasKey(DoctrineTimestampBundle::class, $dependencies);
-        $this->assertArrayHasKey(DoctrineTrackBundle::class, $dependencies);
-
-        $this->assertEquals(['all' => true], $dependencies[DoctrineBundle::class]);
-        $this->assertEquals(['all' => true], $dependencies[DoctrineSnowflakeBundle::class]);
-        $this->assertEquals(['all' => true], $dependencies[DoctrineTimestampBundle::class]);
-        $this->assertEquals(['all' => true], $dependencies[DoctrineTrackBundle::class]);
-    }
 }
